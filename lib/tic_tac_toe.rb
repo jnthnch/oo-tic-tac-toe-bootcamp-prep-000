415a5f8ee4fd1c2aabcbcace5679dbab5d6b69b1
Jonathan Chu <jchu127@gmail.com>
2018-03-09 04:34:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,15 @@ class TicTacToe
     display_board(board)
   end
   
-    
+  def turn_count(board)
+    counter = 0 
+    board.each do |space|
+      if space == "X" || space == "O"
+        counter += 1 
+      end
+    end
+    counter
+  end  
   
   
   
