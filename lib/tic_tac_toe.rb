1a63847ead67cd6b505655227f0c93cf2c25cad7
Jonathan Chu <jchu127@gmail.com>
2018-03-09 04:32:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,6 +49,20 @@ class TicTacToe
     end
   end
   
+  def turn(board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    mark = current_player(board)
+    if valid_move?(board, index)
+    move(board, index, mark)
+    else
+    turn(board)
+    end
+  display_board(board)
+  end
+  
+  
   
   
   
