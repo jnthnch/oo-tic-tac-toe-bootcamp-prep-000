46ddf2996dbde8646fb535d150d1f4eddbff5959
Jonathan Chu <jchu127@gmail.com>
2018-03-09 04:29:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,6 +41,15 @@ class TicTacToe
     end
   end 
   
+  def valid_move?(board, index)
+    if position_taken?(board, index) || !index.between?(0, 8)
+      return false
+    else 
+      return true
+    end
+  end
+  
+  
   
   
 end
